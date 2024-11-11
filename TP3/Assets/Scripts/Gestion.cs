b722d7fd65f6715ded68ee81945eda6abba6e1d7
Ame (Adjamé T.R) <adjame.tel.roz@gmail.com>
2024-11-11 22:24:19 +0100

    Question 4

--- TP3/Assets/Scripts/Gestion.cs
+++ TP3/Assets/Scripts/Gestion.cs
@@ -5,16 +5,17 @@ public class Gestion : MonoBehaviour
 {
     private List<SphereManager> spheres = new List<SphereManager>();
 
+    public float octreeSize = 10; // Taille de l'octree -> la resolution de la grille taille interessante pour les tests entre 5 et 10
+
     void Start()
     {
         // Initialisation de deux sphères
         
-        SphereManager sphere1 = new SphereManager(Vector3.zero, 5.0f, 10.0f, 20);
-        SphereManager sphere2 = new SphereManager(new Vector3(6.0f, 0.0f, 0.0f), 5.0f, 10.0f, 20);
-        //float radius = 5f;
-        //SphereManager sphere1 = new SphereManager(new Vector3(0, 0, 0), radius, 20f, 4);
-        //SphereManager sphere2 = new SphereManager(new Vector3(3, 0, 0), radius, 20f, 4);
+        SphereManager sphere1 = new SphereManager(Vector3.zero, 5.0f, octreeSize, 20);
+        SphereManager sphere2 = new SphereManager(new Vector3(6.0f, 0.0f, 0.0f), 5.0f, octreeSize, 20);
 
+        // Affiche les voxels en union et en intersection
+        
         spheres.Add(sphere1);
         spheres.Add(sphere2);
 
